448fc300115054630f907bd1a25ca228eae8157e
nakatsuji masato <nakatsuji.sak19@gmail.com>
2025-03-15 08:52:31 +0900

    Support for specifying relative path in import ~ from.

--- bin/Front.ts
+++ bin/Front.ts
@@ -1,17 +1,39 @@
 const platform : string = "{{platform}}";
 class FrontControl {
+    private static __root : string;
     private  __fn = {};
     private  __fn_static = {};
     public setFn(name : string, callback : Function) {
         this.__fn[name] = callback;
     }
     public getFn(name : string) {
+        if (FrontControl.__root && name.indexOf(".") === 0) {
+            const names = name.split("/");
+            let namePaths = FrontControl.__root.split("/");
+            namePaths.pop();
+            for(let n = 0 ; n < names.length ; n++) {
+                const n_ = names[n];
+                if (n_ == ".") {
+                    continue;
+                }
+                else if (n_ == "..") {
+                    namePaths.pop();
+                }
+                else {
+                    namePaths.push(n_);
+                }
+            }
+            name = namePaths.join("/");
+        }
         if (this.__fn_static[name]) return this.__fn_static[name];
         if (!this.__fn[name]) {
             console.error("No data available. Check if the file exists in the source file \"" + name + "\".");
             return;
         }
+        const beforeName = FrontControl.__root;
+        FrontControl.__root = name;
         let buffer = this.__fn[name]();
+        FrontControl.__root = beforeName;
         if (typeof buffer == "object") {
             const c = Object.keys(buffer);
             for(let n = 0 ; n < c.length; n++) {
